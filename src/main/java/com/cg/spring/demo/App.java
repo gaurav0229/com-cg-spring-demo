7d35b165d2100f1dd3a4ad798bd099389e7057d4
gaurav0229 <gauravsonawane0229@gmail.com>
2021-06-30 15:37:00 +0530

    commit
    commit

--- src/main/java/com/cg/spring/demo/App.java
+++ src/main/java/com/cg/spring/demo/App.java
@@ -1,13 +1,22 @@
 package com.cg.spring.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author Gaurav
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+		Employee emp = context.getBean(Employee.class);
+		emp.work();
+
+//		emp.name="Some name";
+		System.out.println(emp.ename);
+		System.out.println("Stop");
+	}
 }
--- src/main/java/com/cg/spring/demo/Employee.java
+++ src/main/java/com/cg/spring/demo/Employee.java
@@ -2,4 +2,18 @@ package com.cg.spring.demo;
 
 public class Employee {
 
+	public String ename;
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public void work() {
+		System.out.println("New Employee Works.....");
+	}
+
 }
