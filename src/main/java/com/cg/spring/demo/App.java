be59be85696c80347d1a5c18c8e8ef33bc4c2729
gaurav0229 <gauravsonawane0229@gmail.com>
2021-06-30 18:29:32 +0530

    commit
    commit

--- src/main/java/com/cg/spring/demo/App.java
+++ src/main/java/com/cg/spring/demo/App.java
@@ -9,19 +9,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  */
 public class App {
-	
 	public static void main(String[] args) {
 		System.out.println("Start");
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
 		Employee emp = context.getBean("employee", Employee.class);
 		emp.work();
-		System.out.println(emp.ename);
+		System.out.println(emp.getEname());
+
 		Employee emp2 = context.getBean("employee2", Employee.class);
-		emp2.work();
-		System.out.println(emp2.ename);
-		System.out.println("Stop");
-		
+		System.out.println(emp2.getEname());
+
+		Employee emp3 = context.getBean("employee3", Employee.class);
+		System.out.println(emp3.toString());
+
+		Employee emp4 = context.getBean("employee4", Employee.class);
+		System.out.println(emp4.toString());
+
+		System.out.println("End");
 		((AbstractApplicationContext) context).close();
 	}
 }
